4d90fe16d32b4c45881c25b837e952d3e6806606
jaime <jaime.castillo.oficial@gmail.com>
2026-04-19 21:22:29 +0200

    Added RoutineExerciseActivity and RoutineExerciseViewModel. Created activity_routine_exercise and item_routine_exercise layouts. Refactored date picker logic in DietFragment and updated dialog_select_date styles.

---
+++ app/src/main/java/com/TFG_JCF/fittrack/ui/MainApp/Workout/RoutineExercise/RoutineExerciseActivity.kt
@@ -0,0 +1,21 @@
+package com.TFG_JCF.fittrack.ui.MainApp.Workout.RoutineExercise
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.TFG_JCF.fittrack.R
+
+class RoutineExerciseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_routine_exercise)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ app/src/main/java/com/TFG_JCF/fittrack/ui/MainApp/Workout/RoutineExercise/RoutineExerciseViewModel.kt
@@ -0,0 +1,4 @@
+package com.TFG_JCF.fittrack.ui.MainApp.Workout.RoutineExercise
+
+class RoutineExerciseViewModel {
+}
